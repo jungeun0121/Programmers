f2651ba01eb412e662a91f30f6768730112baa83
jungeun0121 <dodamdodam121@gmail.com>
2021-01-17 23:41:31 +0900

    Level 1_20210114

--- Level 1/[연습문제] 정수 제곱근 판별/solution.py
+++ Level 1/[연습문제] 정수 제곱근 판별/solution.py
@@ -1,11 +1,8 @@
-def solution(participant, completion):
-    
-    participant.sort()
-    completion.sort()
-    
-    for p,c in zip(participant, completion):
-        
-        if p != c:
-            return p
-        
-    return participant.pop()
+import math
+# sqrt(x) : x의 제곱근 반환
+# modf(x) : x의 (소수, 정수) 반환
+# pow(x,y) : x의 y승 반환
+
+def solution(n):
+
+    return math.pow(math.sqrt(n)+1, 2) if n > 0 and math.modf(math.sqrt(n))[0]  == 0 else -1
